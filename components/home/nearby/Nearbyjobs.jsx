33b67024ae73114dd8a612f90af012e0d2578c44
dev.amit.dixit <dev.amit.dixit@gmail.com>
2023-04-04 21:26:53 +0530

    first commit

---
+++ components/home/nearby/Nearbyjobs.jsx
@@ -0,0 +1,52 @@
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import { useRouter } from "expo-router";
+
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
+import { COLORS, icons, SIZES } from "../../../constants";
+
+import styles from "./nearbyjobs.style";
+
+import useFetch from "../../../hook/useFetch";
+
+const Nearbyjobs = () => {
+  const router = useRouter();
+
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}> Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size={SIZES.large} color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went rong</Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
